e0acdc364c65e718caab655aca7082eb8e2e1ad4
Morten Nielsen <morten@morkeleb.com>
2017-01-01 21:25:37 +0100

    removing todos

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 
 
 const program = require('commander');
-
-// TODO: Optionize
-// TODO: Create setup function
-
 const pjson = require('./package.json');
 
 program
@@ -14,5 +10,3 @@ program
   .command('init [config_file]', 'creates a config file template')
   .command('unleash [config_file]', 'starts the monkey.', {isDefault: true})
   .parse(process.argv);
-
-//TODO break this apart!
